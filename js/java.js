925b3c2ecf13d60d4318fa486705e2d9442693fa
jeromeloutsch <151778575+jeromeloutsch@users.noreply.github.com>
2023-12-06 11:47:51 +0100

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,24 +1,13 @@
-function afficherBoiteDialogue() {
+function afficherBarNav() {
     if (window.matchMedia("(max-width: 600px)").matches) {
-        // Afficher l'overlay (fond obscur)
+        // Afficher la bar nav version mobile
         document.getElementById("overlay").style.display = "flex";
     }
 }
 
-function cacherBoiteDialogue() {
+function cacherBarNav() {
     if (window.matchMedia("(max-width: 600px)").matches) {
-        // Cacher l'overlay (fond obscur)
+        // Cahcer la bar nav version mobile
         document.getElementById("overlay").style.display = "none";
-    }
-}
-
-function enleverHTML() {
-    // Vérifier la largeur de l'écran avec une media query en JavaScript
-    if (window.matchMedia("(max-width: 600px)").matches) {
-        // Récupérer l'élément par son ID
-        var modalElement = document.getElementById("modal");
-
-        // Vider le contenu HTML
-        modalElement.innerHTML = "";
     }
 }
